bcd6c8192fcf41f23bd5d08df6c4318c93021ff0
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-19 18:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+let info = document.getElementById("browser-info");
+
+
+// info.innerText= "You are using " + navigator.userAgent;
+
+// info.innerText= "You are using " + navigator.userAgent  + " version " // passing in with this as well
+
+info.innerText = "You are using " + navigator.appName + " version " + navigator.appVersion;
